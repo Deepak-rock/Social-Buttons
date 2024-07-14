704f87d65b4beabab1f3e38c95d8f923e9de5b60
Deepak-rock <deepakind777@gmail.com>
2024-07-14 07:34:23 +0530

    second-commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const Button = props => {
   const {name, color} = props
-  return <button className={color}>{name}</button>
+  return <button className={`button ${color}`}>{name}</button>
 }
 
 const element = (
+<div className = 'container'>
   <div className='bg-img-con'>
     <h1 className='heading'>Social Button</h1>
-    <Button name='like' color='yellow' />
-    <Button name='Comment' color='white' />
-    <Button name='Share' color='blue' />
+    <div className = "button-con ">
+      <Button name='like' color='yellow' />
+      <Button name='Comment' color='white' />
+      <Button name='Share' color='blue' />
+    </div>
   </div>
+</div>
 )
 
 ReactDOM.render(element, document.getElementById('root'))
